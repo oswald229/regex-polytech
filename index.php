45e4e374741d0f7ed76e3f1b223f7c734c9c2590
Mbola <phales.harrison@gmail.com>
2020-01-22 17:46:19 +0100

    Commit index.

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!doctype html>
 <html class="no-js" lang="zxx">
 
@@ -104,52 +103,3 @@
 
     <script src="js/main.js"></script>
 </body>
-=======
-<html>
-
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
-require 'classes/event.php';
-require 'classes/noeud.php';
-require 'classes/arbre.php';
-
-require 'process/functions.php';
-
-?>
-
-
-
-<body>
-
-
-
-
-<h2>Regex Project</h2>
-<br>
-
-
-
-<?php
-	$events_array = parse_fichier("file/timetable (copie).txt");
-
-	$parMatiere = get_par_matiere($events_array);
-
-	$regex = get_regexp($events_array);
-
-
-
-	$regex = get_regexp($parMatiere['Securite informatique']);
-
-	echo $regex;
-?>
-
-
-
-
-</body>
-
-
-</html>
->>>>>>> 94249ddcd297d035ff11545378eecd547d277f3a
