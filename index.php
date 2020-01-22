94249ddcd297d035ff11545378eecd547d277f3a
usrlocal <usrlocal@GE22-FE20.ad.univ-angers.fr>
2020-01-22 17:31:05 +0100

    Setup some html.

--- index.php
+++ index.php
@@ -13,43 +13,13 @@ require 'process/functions.php';
 ?>
 
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-
 
 <body>
 
 
 
-<div class="container">
-<br><br>
+
 <h2>Regex Project</h2>
-<br>
-
-<div class="input-group mb-3">
-  <div class="input-group-prepend">
-    <span class="input-group-text" id="basic-addon1">Lien ics</span>
-  </div>
-  <input type="text" class="form-control" placeholder="Url ics" aria-describedby="basic-addon1">
-</div>
-<br>
-
-
-
-<div class="input-group">
-
-  <div class="custom-file">
-    	<input type="file" class="custom-file-input" accept=".ics,.txt" id="inputGroupFile04">
-    	<label class="custom-file-label" for="inputGroupFile04">Choose file</label>
-  </div>
-  
-	
-</div>
-
-<br>
-
-<button type="button" class="btn btn-primary">Get selection</button>
-
 <br>
 
 
